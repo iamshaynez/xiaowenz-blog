bfd38933ea2c44679c53b2a629428d211a834270
Xiaowen.Z <xiaowen.z@outlook.com>
2025-09-30 22:39:23 +0800

    update

--- read.py
+++ read.py
@@ -114,7 +114,7 @@ def get_next_book_number(filename):
 
 
 if __name__ == "__main__":
-    url = 'https://book.douban.com/subject/36657388/'
+    url = 'https://book.douban.com/subject/36488645/'
 
     process(url)
     #count = get_next_book_number('./content/read/read-2024.md')
